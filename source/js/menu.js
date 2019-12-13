7c816488403539d62221dc773e5813967a31b1eb
Dmitry Kuchukov <kudim84@yandex.ru>
2019-12-14 00:45:33 +0300

    modify js files

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
--- source/js/popup.js
+++ source/js/popup.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const form = document.querySelector('.form');
-const inputsForm = form.querySelectorAll("input");
-const popupSuccess = document.querySelector('.popup--success');
-const popupError = document.querySelector('.popup--failure');
-const button = document.querySelector('.popup__button');
-const popup = document.querySelector('.popup');
-const formButton = document.querySelector('.form__button');
+var form = document.querySelector('.form');
+var inputsForm = form.querySelectorAll("input");
+var popupSuccess = document.querySelector('.popup--success');
+var popupError = document.querySelector('.popup--failure');
+var button = document.querySelector('.popup__button');
+var popup = document.querySelector('.popup');
+var formButton = document.querySelector('.form__button');
 
-let showPopupOfValidity = function (element) {
+var showPopupOfValidity = function (element) {
   if (element.checkValidity() === false) {
     popupError.style.display = 'block';
   } else {
@@ -16,7 +16,7 @@ let showPopupOfValidity = function (element) {
   }
 };
 
-let checkElementForm = function (elements) {
+var checkElementForm = function (elements) {
   elements.forEach(function (item) {
     showPopupOfValidity(item);
   });
