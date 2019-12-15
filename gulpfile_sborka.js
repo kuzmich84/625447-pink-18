0a07674b5b564ebdf83536c105b5e1b505d1bec2
kuzmich84 <kudim@inbox.ru>
2019-12-16 00:11:05 +0300

    add webp and retina

--- gulpfile_sborka.js
+++ gulpfile_sborka.js
@@ -43,7 +43,8 @@ gulp.task("images", function () {
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img")); });
+    .pipe(gulp.dest("source/img"));
+});
 
 
 gulp.task("html", function () {
